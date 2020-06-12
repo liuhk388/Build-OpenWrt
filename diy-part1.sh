c82c693b112c3e77e218a84d2f3fa6809c909d43
Hongkang Liu <liuhk388@gmail.com>
2020-06-12 22:36:01 +0800

    Update from upstream
    Test update from upstream

---
+++ diy-part1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
